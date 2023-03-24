d9aee80f1c2e26e54e68e90093806ab3d8bc64cf
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-03-25 01:56:44 +0700

    belajar request header

---
+++ header_test.go
@@ -0,0 +1,26 @@
+package belajar_golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func RequestHeader(writer http.ResponseWriter, request *http.Request) {
+	contentType := request.Header.Get("content-type")
+	fmt.Fprint(writer, contentType)
+}
+
+func TestRequestHeader(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost/", nil)
+	request.Header.Add("Content-Type", "application/json") // case insensitive (content-type)
+	recorder := httptest.NewRecorder()
+
+	RequestHeader(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(string(body))
+}
