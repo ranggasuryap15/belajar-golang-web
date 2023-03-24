7d5360ceea40b7b3140179a6ce51dc4293c47b9d
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-03-24 23:38:43 +0700

    belajar query multiple values

--- query_param_test.go
+++ query_param_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -41,6 +42,24 @@ func TestMultipleQueryParameter(t *testing.T) {
 	recorder := httptest.NewRecorder()
 
 	MultipleQueryParameter(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(string(body))
+}
+
+func MultipleParameterValues(writer http.ResponseWriter, request *http.Request) {
+	query := request.URL.Query()
+	names := query["name"] // jika menggunakan get, maka cuman mendapatkan value yang paling pertama saja.
+
+	fmt.Fprint(writer, strings.Join(names, " "))
+}
+
+func TestMultipleParameterValues(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello?name=Rangga&name=Surya", nil)
+	recorder := httptest.NewRecorder()
+
+	MultipleParameterValues(recorder, request)
 
 	response := recorder.Result()
 	body, _ := io.ReadAll(response.Body)
