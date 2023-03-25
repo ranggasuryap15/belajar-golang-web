cfd51804138d3b236787c3949bcf144e14da52b7
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-03-26 00:09:42 +0700

    membuat file server dengan golang embed

--- file_server_test.go
+++ file_server_test.go
@@ -1,6 +1,8 @@
 package belajar_golang_web
 
 import (
+	"embed"
+	"io/fs"
 	"net/http"
 	"testing"
 )
@@ -9,6 +11,28 @@ import (
 func TestFileServer(t *testing.T) {
 	directory := http.Dir("./resources")
 	fileServer := http.FileServer(directory)
+
+	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+//go:embed resources
+var resources embed.FS
+
+// dengan menggunakan embed itu tidak perlu copy paste static filenya ke server, tinggal menggunakan distribution file-nya saja
+func TestFileServerGolangEmbed(t *testing.T) {
+	directory, _ := fs.Sub(resources, "resources")
+	fileServer := http.FileServer(http.FS(directory))
 
 	mux := http.NewServeMux()
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
