bb5400804c91899d5ce771b167c7febe5c263ee7
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-03-26 01:13:02 +0700

    membuat serve file dengan embed tanpa perlu serve file

--- serve_file_test.go
+++ serve_file_test.go
@@ -1,6 +1,8 @@
 package belajar_golang_web
 
 import (
+	_ "embed"
+	"fmt"
 	"net/http"
 	"testing"
 )
@@ -17,6 +19,33 @@ func TestServeFileServer(t *testing.T) {
 	server := http.Server{
 		Addr:    "localhost:8080",
 		Handler: http.HandlerFunc(ServeFile),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+//go:embed resources/ok.html
+var resourceOK string
+
+//go:embed resources/notfound.html
+var resourceNotFound string
+
+func ServeFileEmbed(writer http.ResponseWriter, request *http.Request) {
+	// lebih simpel dibandingkan tanpa menggunakan embed
+	if request.URL.Query().Get("name") != "" {
+		fmt.Fprint(writer, resourceOK)
+	} else {
+		fmt.Fprint(writer, resourceNotFound)
+	}
+}
+
+func TestServeFileServerEmbed(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFileEmbed),
 	}
 
 	err := server.ListenAndServe()
