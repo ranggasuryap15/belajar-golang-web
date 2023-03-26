ce113f4b91c69246499be4e576ed12c18fb9648d
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-03-27 00:56:51 +0700

    template function global bawaan golang

--- template_function_test.go
+++ template_function_test.go
@@ -33,3 +33,20 @@ func TestTemplateFunction(t *testing.T) {
 	body, _ := io.ReadAll(response.Body)
 	fmt.Println(string(body))
 }
+
+func TemplateFunctionGlobal(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.New("FUNCTION").Parse(`{{len .Name}}`))
+	t.ExecuteTemplate(writer, "FUNCTION", MyPage{
+		Name: "Rangga",
+	})
+}
+
+func TestTemplateFunctionGlobal(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateFunctionGlobal(recorder, request)
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(string(body))
+}
