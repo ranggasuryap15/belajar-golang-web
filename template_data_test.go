e7420f19b01356cbd2fee475d3676cd00b9e9634
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-03-26 14:38:45 +0700

    membuat template data dengan struct

--- template_data_test.go
+++ template_data_test.go
@@ -29,3 +29,37 @@ func TestTemplateDataMap(t *testing.T) {
 
 	fmt.Println(string(body))
 }
+
+type Page struct {
+	Title   string
+	Name    string
+	Address Address
+}
+
+type Address struct {
+	Street string
+}
+
+func TemplateDataStruct(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/name.gohtml"))
+
+	t.ExecuteTemplate(writer, "name.gohtml", Page{
+		Title: "Template Data Struct",
+		Name:  "Rangga",
+		Address: Address{
+			Street: "Jl Kaum 2",
+		},
+	})
+}
+
+func TestTemplateDataStruct(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateDataStruct(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
