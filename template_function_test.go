aa15eba3fbbae358c564e646ccca3701f3a0ad86
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-03-27 00:26:59 +0700

    template function

---
+++ template_function_test.go
@@ -0,0 +1,35 @@
+package belajar_golang_web
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type MyPage struct {
+	Name string
+}
+
+func (myPage MyPage) SayHello(name string) string {
+	return "Hello " + name + ", My Name is " + myPage.Name
+}
+
+func TemplateFunction(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.New("FUNCTION").Parse(`{{.SayHello "Budi"}}`))
+	t.ExecuteTemplate(writer, "FUNCTION", MyPage{
+		Name: "Rangga",
+	})
+}
+
+func TestTemplateFunction(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateFunction(recorder, request)
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(string(body))
+}
