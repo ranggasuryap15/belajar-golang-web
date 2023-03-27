596af9abd917f573956983c86def25429b51643a
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-03-27 14:20:44 +0700

    template escape disabled

--- xss_test.go
+++ xss_test.go
@@ -2,6 +2,7 @@ package belajar_golang_web
 
 import (
 	"fmt"
+	"html/template"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -28,11 +29,28 @@ func TestTemplateAutoEscape(t *testing.T) {
 func TestTemplateAutoEscapeServer(t *testing.T) {
 	server := http.Server{
 		Addr:    "localhost:8080",
-		Handler: http.HandlerFunc(TemplateAutoEscape),
+		Handler: http.HandlerFunc(TemplateAutoEscapeDisabled),
 	}
 
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(err)
 	}
+}
+
+func TemplateAutoEscapeDisabled(writer http.ResponseWriter, request *http.Request) {
+	myTemplates.ExecuteTemplate(writer, "post.gohtml", map[string]interface{}{
+		"Title": "Auto Escape HTML Disable",
+		"Body":  template.HTML("<p>Template Auto Escape dimatikan</p>"),
+	})
+}
+
+func TestTemplateAutoEscapeDisabled(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateAutoEscapeDisabled(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
 }
